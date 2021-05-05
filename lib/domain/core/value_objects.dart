502f27d76c55b32b35b71048049f78e403ce0bc7
Fathan Soeltoni <fsoeltoni@gmail.com>
2021-05-06 00:23:52 +0700

    Create user entity

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:incharge/domain/core/errors.dart';
 import 'package:incharge/domain/core/failures.dart';
+import 'package:uuid/uuid.dart';
 
 @immutable
 abstract class ValueObject<T> {
@@ -25,4 +26,24 @@ abstract class ValueObject<T> {
 
   @override
   String toString() => 'ValueObject(value: $value)';
+}
+
+@immutable
+class UniqueId extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory UniqueId() {
+    return UniqueId._(
+      right(const Uuid().v1()),
+    );
+  }
+
+  factory UniqueId.fromUniqueString(String uniqueId) {
+    return UniqueId._(
+      right(uniqueId),
+    );
+  }
+
+  const UniqueId._(this.value);
 }
