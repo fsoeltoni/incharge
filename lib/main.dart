d69213fde98095d80930b4491330b144cff1a650
Fathan Soeltoni <fsoeltoni@gmail.com>
2021-05-05 22:32:14 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:incharge/presentation/incharge.dart';
+
+void main() {
+  runApp(const Incharge());
+}
---
+++ lib/presentation/incharge.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Incharge extends StatelessWidget {
+  const Incharge() : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("INCHARGE"),
+        ),
+      ),
+    );
+  }
+}
